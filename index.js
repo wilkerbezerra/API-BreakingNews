b80ae24e9cd90d9df1fd4650e343430a4c0e6239
wilkerbezerra <wilker.bezerra1000@gmail.com>
2025-11-01 15:12:17 -0300

    Fim da Aula 03

--- index.js
+++ index.js
@@ -1,9 +1,16 @@
 const express = require("express");
-
 const app = express();
 
-app.get("/", (req, res) => {
-  res.send("Hello World");
+//ROTA
+//Método HTTP
+//Name
+//Function (callback)
+// app.get("/", (req, res) => {
+//   res.send("Hello World");
+// });
+app.get("/soma", (req, res) => {
+  const soma = 100 + 1;
+  res.send({ soma: soma });
 });
 
 app.listen(3000);
